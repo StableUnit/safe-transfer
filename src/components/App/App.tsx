5ef79290e1bada369f766c23126976de77cb741a
Дмитрий <mitya-kudr@mail.ru>
2023-01-21 16:55:55 +0300

    fix: provider for web3 + restore styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,8 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import Web3 from "web3";
 
 import { useWeb3Modal } from "@web3modal/react";
-import { useAccount, useDisconnect, useChainId, useNetwork, useProvider } from "wagmi";
+import { useAccount, useDisconnect, useNetwork } from "wagmi";
 
 import Header from "../Header/Header";
 import { Footer } from "../Footer/Footer";
@@ -14,24 +14,21 @@ import { BugIcon } from "../../ui-kit/images/icons";
 import "./App.scss";
 
 const App = () => {
-    const [web3, setWeb3] = useState(new Web3(Web3.givenProvider));
     const dispatch = useContext(DispatchContext);
     const { open } = useWeb3Modal();
     const { disconnect } = useDisconnect();
 
-    const { address, status, isConnected } = useAccount();
+    const { address } = useAccount();
     const { chain } = useNetwork();
-    const provider = useProvider();
 
     const syncStore = async () => {
         dispatch({ type: Actions.SetCurrentAddress, payload: address });
         dispatch({ type: Actions.SetChainId, payload: chain?.id });
-        dispatch({ type: Actions.SetWeb3, payload: new Web3(provider) });
+        dispatch({ type: Actions.SetWeb3, payload: new Web3(Web3.givenProvider) });
     };
     useEffect(() => {
-        console.log(isConnected, status, connector, chain);
         syncStore();
-    }, [address, status, connector, chain, isConnected]);
+    }, [address, chain]);
 
     const onConnect = async () => {
         await open();
