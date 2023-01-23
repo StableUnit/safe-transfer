388d625a91df5dae22cafae19426600644cd846b
Дмитрий <mitya-kudr@mail.ru>
2023-01-23 14:53:23 +0300

    feat: fix events

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,17 +1,26 @@
 import amplitude from "amplitude-js";
 import axios from "axios";
 
-const BACKEND_URL = "http://146.190.27.205:4000/";
+const BACKEND_URL = "http://146.190.27.205:4000";
 
 export const trackEvent = (event: string, data?: Record<string, any>) => {
     amplitude.getInstance().logEvent(event, data);
 };
 
 export const sendAddTransferEvent = async (data?: Record<string, any>) => {
-    await axios.post(`${BACKEND_URL}/v1/addTransfer`, data);
+    try {
+        await axios.post(`${BACKEND_URL}/v1/addTransfer`, data);
+    } catch (e: any) {
+        console.error(e.message);
+    }
 };
 
 export const getVolume = async () => {
-    const response = await axios.get(`${BACKEND_URL}/v1/getVolume`);
-    return response.data;
+    try {
+        const response = await axios.get(`${BACKEND_URL}/v1/getVolume`);
+        return response.data;
+    } catch (e: any) {
+        console.error(e.message);
+        return 0;
+    }
 };
