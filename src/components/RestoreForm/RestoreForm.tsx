f117474853ac9412a5d1e8d5bb2b489bb66068b4
Дмитрий <mitya-kudr@mail.ru>
2023-02-06 15:17:55 +0300

    fix: restore form

--- src/components/RestoreForm/RestoreForm.tsx
+++ src/components/RestoreForm/RestoreForm.tsx
@@ -1,65 +1,49 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { TextField } from "@mui/material";
+import Web3 from "web3";
 
-import { useAccount, useNetwork, useTransaction } from "wagmi";
+import { useAccount, useNetwork } from "wagmi";
 import { idToNetwork } from "../../utils/network";
 import { addErrorNotification } from "../../utils/notifications";
 import Button from "../../ui-kit/components/Button/Button";
+import GenUrl from "../GenUrl";
+import { generateUrl } from "../../utils/urlGenerator";
 
 import "./RestoreForm.scss";
-import GenUrl from "../GenUrl";
 
 const RestoreForm = () => {
     const { address } = useAccount();
     const { chain } = useNetwork();
+    const { connector } = useAccount();
     const networkName = chain?.id ? idToNetwork[chain?.id] : undefined;
     const [restoreHash, setRestoreHash] = useState<undefined | string>(undefined);
     const [isRestoreLoading, setIsRestoreLoading] = useState(false);
     const [genUrl, setGenUrl] = useState<undefined | string>(undefined);
 
-    // @ts-ignore
-    const { data: transactionData } = useTransaction({ hash: restoreHash });
-
     const handleRestoreHashChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setRestoreHash(event.target.value);
     };
 
-    useEffect(() => {
-        if (transactionData) {
-            console.log(transactionData);
-            // const valueBN = Web3.utils.hexToNumberString(transactionData..logs[0].data);
-            // const to = `0x${transaction.logs[0].topics[2]?.slice(-40)}`;
-            // setGenUrl(
-            //   generateUrl({
-            //       address: transactionData.to,
-            //       from: transactionData.from,
-            //       to,
-            //       value: valueBN,
-            //       chain: networkName,
-            //   })
-            // );
-        }
-    }, [transactionData]);
-
     const handleRestore = async () => {
         try {
+            const web3 = new Web3(await connector?.getProvider());
+            const transaction = await web3?.eth.getTransactionReceipt(restoreHash ?? "");
+            if (!transaction || !networkName) {
+                addErrorNotification("Error", "Can't get transaction");
+                return;
+            }
             setIsRestoreLoading(true);
-            // if (!transaction || !networkName) {
-            //     addErrorNotification("Error", "Can't get transaction");
-            //     setIsRestoreLoading(false);
-            //     return;
-            // }
-            // const valueBN = Web3.utils.hexToNumberString(transaction.logs[0].data);
-            // const to = `0x${transaction.logs[0].topics[2]?.slice(-40)}`;
-            // setGenUrl(
-            //     generateUrl({
-            //         address: transaction.to,
-            //         from: transaction.from,
-            //         to,
-            //         value: valueBN,
-            //         chain: networkName,
-            //     })
-            // );
+            const valueBN = Web3.utils.hexToNumberString(transaction.logs[0].data);
+            const to = `0x${transaction.logs[0].topics[2]?.slice(-40)}`;
+            setGenUrl(
+                generateUrl({
+                    address: transaction.to,
+                    from: transaction.from,
+                    to,
+                    value: valueBN,
+                    chain: networkName,
+                })
+            );
             setIsRestoreLoading(false);
         } catch (e) {
             setIsRestoreLoading(false);
@@ -74,7 +58,7 @@ const RestoreForm = () => {
                 Please ask sender to send you a link with receive details. <br />
                 Alternatively, you can use widget below to restore details from on-chain data.
             </div>
-            <div className="send-form">
+            <div className="send-form restore-form">
                 <div className="restore-form__restore">
                     <div>Restore link by Approve transaction hash</div>
                     <TextField
