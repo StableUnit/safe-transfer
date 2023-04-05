63c8c37ce79e0c721b36f45f1ecc4a81d3df2e8a
Дмитрий <mitya-kudr@mail.ru>
2023-04-05 11:42:46 +0300

    feat: remove bug button

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,6 @@ import cn from "classnames";
 import Header from "../Header/Header";
 import { Footer } from "../Footer/Footer";
 import { Routes } from "../Routes";
-import { BugIcon } from "../../ui-kit/images/icons";
 import { ReactComponent as MetamaskIcon } from "../../ui-kit/images/metamask-circle.svg";
 import { ReactComponent as WalletConnectIcon } from "../../ui-kit/images/walletconnect.svg";
 import { ReactComponent as GnosisSafeIcon } from "../../ui-kit/images/gnosis-safe.svg";
@@ -46,14 +45,6 @@ const App = () => {
         closeModal();
     };
 
-    const onBugClick = () => {
-        window.open(
-            // eslint-disable-next-line max-len
-            "https://github.com/StableUnit/safe-transfer/issues/new?assignees=Kud8&labels=&template=bug_report.md&title=%5BBUG%5D",
-            "_blank"
-        );
-    };
-
     const renderIcon = (id: string) => {
         switch (id.toLowerCase()) {
             case "metamask":
@@ -73,7 +64,6 @@ const App = () => {
             <div className="App__container">
                 <Routes onConnect={openModal} />
             </div>
-            <BugIcon className="bug-icon" id="bug-icon" onClick={onBugClick} />
             <Footer />
             {isModalVisible && (
                 <div className="connect-modal">
