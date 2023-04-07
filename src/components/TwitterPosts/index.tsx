c788a10086d5b16e1b32b86ea705d69361b4b55f
Дмитрий <mitya-kudr@mail.ru>
2023-04-07 10:26:32 +0300

    fix: twitter screenshots in mobile

--- src/components/TwitterPosts/index.tsx
+++ src/components/TwitterPosts/index.tsx
@@ -22,15 +22,20 @@ export const TwitterPosts = ({ ids }: Props) => (
             // when window width is >= 0px
             0: {
                 slidesPerView: 1,
-                spaceBetween: 20,
+                spaceBetween: 15,
             },
             // when window width is >= 480px
             480: {
                 slidesPerView: 2,
-                spaceBetween: 30,
+                spaceBetween: 20,
             },
             // when window width is >= 640px
             640: {
+                slidesPerView: 3,
+                spaceBetween: 15,
+            },
+            // when window width is >= 1000px
+            1000: {
                 slidesPerView: 4,
                 spaceBetween: 15,
             },
