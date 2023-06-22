35ba2774aa61dd2a27c5a6935df1542dae97565c
Дмитрий <mitya-kudr@mail.ru>
2023-06-22 15:10:43 +0300

    feat: add Argent button

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useConnect, useDisconnect } from "wagmi";
+import { Connector, useConnect, useDisconnect } from "wagmi";
 import cn from "classnames";
 
 import Header from "../Header/Header";
@@ -8,6 +8,7 @@ import { Routes } from "../Routes";
 import { ReactComponent as MetamaskIcon } from "../../ui-kit/images/metamask-circle.svg";
 import { ReactComponent as WalletConnectIcon } from "../../ui-kit/images/walletconnect.svg";
 import { ReactComponent as GnosisSafeIcon } from "../../ui-kit/images/gnosis-safe.svg";
+import { ReactComponent as ArgentIcon } from "../../ui-kit/images/argent.svg";
 import { trackEvent } from "../../utils/events";
 import { useAutoConnect } from "../../hooks/useAutoConnect";
 import { StateContext } from "../../reducer/constants";
@@ -16,12 +17,22 @@ import "./App.scss";
 
 const App = () => {
     const { uiSelectedChainId } = useContext(StateContext);
+    const [extendedConnectors, setExtendedConnectors] = useState<Connector<any, any, any>[]>([]);
     const { connectAsync, connectors, isLoading, pendingConnector } = useConnect({ chainId: uiSelectedChainId });
     const { disconnect } = useDisconnect();
 
     const [isModalVisible, setIsModalVisible] = useState(false);
 
     useAutoConnect();
+
+    useEffect(() => {
+        const walletConnect = connectors.find((v) => v.id.toLowerCase() === "walletconnectlegacy");
+        if (walletConnect) {
+            setExtendedConnectors([...connectors, walletConnect]);
+        } else {
+            setExtendedConnectors(connectors);
+        }
+    }, [connectors]);
 
     useEffect(() => {
         trackEvent("pageLoaded", { location: window.location.href });
@@ -52,6 +63,8 @@ const App = () => {
             case "walletconnect":
             case "walletconnectlegacy":
                 return <WalletConnectIcon />;
+            case "argent":
+                return <ArgentIcon />;
             case "safe":
                 return <GnosisSafeIcon />;
             default:
@@ -78,25 +91,27 @@ const App = () => {
             {isModalVisible && (
                 <div className="connect-modal">
                     <div className="connect-modal__card">
-                        {connectors.map((selectedConnector) => (
-                            <div className="connect-modal__provider-wrapper" key={selectedConnector.id}>
-                                <div
-                                    className={cn("connect-modal__provider-container", {
-                                        "connect-modal__provider-container--disabled": !selectedConnector.ready,
-                                    })}
-                                    key={selectedConnector.id}
-                                    onClick={handleConnect(selectedConnector)}
-                                >
-                                    <div className="connect-modal__provider-icon">
-                                        {renderIcon(selectedConnector.id)}
-                                    </div>
-                                    <div className="connect-modal__provider-name">
-                                        {getConnectorName(selectedConnector.name)}
-                                        {isLoading && selectedConnector.id === pendingConnector?.id && " (connecting)"}
+                        {extendedConnectors.map((selectedConnector, i) => {
+                            const id = i === extendedConnectors.length - 1 ? "Argent" : selectedConnector.id;
+                            const name = i === extendedConnectors.length - 1 ? "Argent" : selectedConnector.name;
+                            return (
+                                <div className="connect-modal__provider-wrapper" key={id}>
+                                    <div
+                                        className={cn("connect-modal__provider-container", {
+                                            "connect-modal__provider-container--disabled": !selectedConnector.ready,
+                                        })}
+                                        key={id}
+                                        onClick={handleConnect(selectedConnector)}
+                                    >
+                                        <div className="connect-modal__provider-icon">{renderIcon(id)}</div>
+                                        <div className="connect-modal__provider-name">
+                                            {getConnectorName(name)}
+                                            {isLoading && id === pendingConnector?.id && " (connecting)"}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
+                            );
+                        })}
                     </div>
                     <div className="connect-modal__fade" onClick={closeModal} />
                 </div>
